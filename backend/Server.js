27394e1256b78cbc519fa573570ec0d7ed081214
subiksha-venkidusamy <124667477+subiksha-venkidusamy@users.noreply.github.com>
2025-05-16 22:39:51 +0530

    make the cors work for deployed url

--- backend/Server.js
+++ backend/Server.js
@@ -7,13 +7,14 @@ const jwt = require("jsonwebtoken");
 const axios = require("axios");
 require("dotenv").config();
 const app = express();
-const allowedOrigin = process.env.FRONTEND_URL;
+
 app.use(
   cors({
-    origin: allowedOrigin,
+    origin: "*",
     credentials: true,
   })
 );
+
 app.use(bodyParser.json());
 
 const JWT_SECRET = process.env.JWT_SECRET;
